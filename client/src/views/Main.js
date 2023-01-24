b03459737ba46d58d07499c4336ea04fdcf1457d
Christopher Rivera <riveramercado.christopher@gmail.com>
2023-01-24 09:54:43 -0400

    Fixed title name

--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -3,7 +3,7 @@ import ProductForm from '../components/ProductForm';
 export default () => {
     return (
         <div className='container mt-5'>
-            <h1 className='mb-5'>Product Form</h1>
+            <h1 className='mb-5'>Product Manager</h1>
             <ProductForm />
         </div>
     )
