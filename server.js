f8bdc21e2d66fbe09807c8abe5a5b947513f4861
Christopher Rivera <riveramercado.christopher@gmail.com>
2023-01-23 21:09:32 -0400

    Created server/client structure, tested API

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 8000;
-    
-app.listen(port, () => console.log(`Listening on port: ${port}`) );
+
+require('./server/routes/product.routes')(app); // This is new
+
+app.listen(8000, () => {
+    console.log("Listening at Port 8000")
+})
